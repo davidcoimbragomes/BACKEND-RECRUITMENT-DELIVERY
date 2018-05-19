80907cb799639c0692ff05d81b276a04b6ce89ea
David Gomes <david.gomes@worldit.pt>
2018-05-19 18:02:51 +0100

    health_status

---
+++ WebAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http.Routing;
+
+namespace WebAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            //    // Web API configuration and services
+            //    // Configure Web API to use only bearer token authentication.
+            //    config.SuppressDefaultHostAuthentication();
+            //    config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            //    // Web API routes
+            //    config.MapHttpAttributeRoutes();
+
+            //    config.Routes.MapHttpRoute(
+            //        name: "DefaultApi",
+            //        routeTemplate: "api/{controller}/{id}",
+            //        defaults: new { id = RouteParameter.Optional }
+            //    );
+            //}
+
+            config.MapHttpAttributeRoutes();
+
+
+            // D.G: removed "api/{controller}/{id}"
+            // define route
+            IHttpRoute defaultRoute = config.Routes.CreateRoute("{controller}/{id}",
+                                                new { id = RouteParameter.Optional }, null);
+
+            // Add route
+            config.Routes.Add("DefaultApi", defaultRoute);
+        }
+
+    }
+}
---
+++ WebAPI/Controllers/HealthController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class HealthController : ApiController
+    {
+
+        [HttpGet]
+        [ResponseType(typeof(StatusModel))]
+        public IHttpActionResult health()
+        {
+            //Responsetype of type StatusModel because in the example the answer was identical 
+            try
+            {
+                StatusModel response = new StatusModel();
+                response.Status = "OK";
+                return Ok(response);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(new Exception(e.Message));
+            }
+        }
+    }
+}
